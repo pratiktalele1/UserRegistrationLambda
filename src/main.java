d5c08b30f7257214a636ff4d1c667290dcea4acf
Pratik Talele <pratiktalele22@gmail.com>
2021-08-04 17:05:25 +0530

    commiting uc2

--- src/main.java
+++ src/main.java
@@ -1,9 +1,11 @@
 
 public class main {
-public static void main(String[] args)   {
-	//creating uc1 object
-	UC1 uc1=new UC1();
-	uc1.firstName("Pratik");
-	
-}
+	public static void main(String[] args) {
+		// creating uc1 object
+		UC1 uc1 = new UC1();
+		uc1.firstName("Pratik");
+		// creating uc2 object
+		UC2 uc2 = new UC2();
+		uc2.lastName("Talele");
+	}
 }
