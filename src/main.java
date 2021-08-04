5ebc47470350b8bf13da539a1feb74f7842ab8f3
Pratik Talele <pratiktalele22@gmail.com>
2021-08-04 17:06:44 +0530

    commiting uc3

--- src/main.java
+++ src/main.java
@@ -7,5 +7,8 @@ public class main {
 		// creating uc2 object
 		UC2 uc2 = new UC2();
 		uc2.lastName("Talele");
+		//creating uc3 object
+		UC3 uc3=new UC3();
+		uc3.email("abc.xyz@bl.co.in");
 	}
 }
